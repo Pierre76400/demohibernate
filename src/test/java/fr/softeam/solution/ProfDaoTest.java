ae2dcbf644d74abf422d698ef5712d4c81673ce9
Pierre LEPLAY <pierre.leplay@externe.sacem.fr>
2016-09-30 14:03:01 +0200

    Gestion dynamique des log dans les TU

--- src/test/java/fr/softeam/solution/ProfDaoTest.java
+++ src/test/java/fr/softeam/solution/ProfDaoTest.java
@@ -5,13 +5,16 @@ import java.util.HashSet;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import ch.qos.logback.classic.Level;
 import fr.softeam.dao.ProfesseurDao;
 import fr.softeam.model.Classe;
 import fr.softeam.model.Eleve;
@@ -22,49 +25,57 @@ import fr.softeam.model.Professeur;
 @Transactional
 public class ProfDaoTest {
 
-	@Autowired
-	private ProfesseurDao professeurDao;
+    @Autowired
+    private ProfesseurDao professeurDao;
 
-	@Autowired
-	private EntityManager entityManager;
+    @Autowired
+    private EntityManager entityManager;
 
-	@Before
-	public void init() {
-		int cptEleve = 0;
-		for (int i = 0; i < 6; i++) {
-			Classe c = new Classe();
-			c.setNom("classe" + i);
+    @Before
+    public void init() {
+        int cptEleve = 0;
+        for (int i = 0; i < 6; i++) {
+            Classe c = new Classe();
+            c.setNom("classe" + i);
 
-			Professeur p = new Professeur();
-			p.setNom("Professeur" + i);
+            Professeur p = new Professeur();
+            p.setNom("Professeur" + i);
 
-			c.setProfesseur(p);
+            c.setProfesseur(p);
 
-			c.setEleves(new HashSet<Eleve>());
-			for (int j = 0; j < 25; j++) {
-				Eleve e = new Eleve();
-				e.setNom("eleve" + cptEleve++);
-				c.getEleves().add(e);
-			}
-			entityManager.persist(c);
-		}
-	}
+            c.setEleves(new HashSet<Eleve>());
+            for (int j = 0; j < 25; j++) {
+                Eleve e = new Eleve();
+                e.setNom("eleve" + cptEleve++);
+                c.getEleves().add(e);
+            }
+            entityManager.persist(c);
+        }
+    }
 
-	@Test
-	public void firstTest() {
-		Professeur p = new Professeur();
+    @After
+    public void end() {
 
-		p.setNom("Tournesol");
-		p.setPrenom("Triton");
+        ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger("org.hibernate.SQL")).setLevel(Level.ERROR);
+    }
 
-		professeurDao.save(p);
+    @Test
+    public void firstTest() {
+        ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger("org.hibernate.SQL")).setLevel(Level.DEBUG);
 
-		entityManager.clear();
-		entityManager.flush();
+        Professeur p = new Professeur();
 
-		for (Professeur pro : professeurDao.findAll()) {
-			System.out.println(pro.getNom());
-		}
+        p.setNom("Tournesol");
+        p.setPrenom("Triton");
 
-	}
+        professeurDao.save(p);
+
+        entityManager.clear();
+        entityManager.flush();
+
+        for (Professeur pro : professeurDao.findAll()) {
+            System.out.println(pro.getNom());
+        }
+
+    }
 }
